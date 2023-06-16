b238b7a8c126b5737a8ea718fe2a48c9de74b965
AkiraKumoshita <kumo@MacBook-Pro-AK.local>
2023-06-16 10:02:23 +0900

    ver 2.0 fix

--- InvadersGameClient.java
+++ InvadersGameClient.java
@@ -3,7 +3,6 @@ import java.net.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
